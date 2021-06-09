a9dcfbba83cc3a483547fc55ff6f97d03b0fd1bc
descifrador17 <utkarshdayal.17@gmail.com>
2021-06-10 02:06:11 +0530

    Added Card, Dropdown, Alert Demo

--- JetpackComposePlayground/app/src/main/java/com/descifrador/jetpackcomposeplayground/util/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.descifrador.jetpackcomposeplayground.util
-
-object AppConstants{
-    val BASE_URL = "https://pokeapi.co/api/v2/"
-}
--- JetpackComposePlayground/app/src/main/java/com/descifrador/jetpackcomposeplayground/util/DtoMapper.kt
+++
@@ -1,8 +0,0 @@
-package com.descifrador.jetpackcomposeplayground.util
-
-interface DtoMapper<T, DomainModel> {
-    fun mapToDomainModel(entity: T): DomainModel
-
-    fun mapFromDomainModel(domainModel: DomainModel): T
-
-}
